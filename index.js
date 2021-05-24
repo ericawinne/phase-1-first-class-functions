dcd28034f5012eb1ebc7a78d9082f444a216b472
Erica Winne <erica.winne@powerhrg.com>
2021-05-23 22:05:45 -0400

    first class function

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    return callback ()
+}
+function returnsANamedFunction(){
+    return function erica(){
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function() {
+    }
+}
